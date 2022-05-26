da21b6e56f81402378ca510e4ac04a3d06e6de69
jijidraw <robert.jerome89@gmail.com>
2022-05-26 17:28:18 +0200

    updatecloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,19 @@
 module.exports = {
     jwtSecret: process.env.JWT_SECRET
   };
+  module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+    // ...
+  });
